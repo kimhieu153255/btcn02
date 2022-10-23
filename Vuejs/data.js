bd5cee2bb07a6b3d04f7e67c922be644afe72d05
20120474 <84512387+kimhieu153255@users.noreply.github.com>
2022-10-23 19:36:47 +0700

    add data.js

---
+++ Vuejs/data.js
@@ -0,0 +1,15 @@
+export default class {
+  constructor(obj) {
+    this.id = obj.id;
+    this.rank = obj.rank;
+    this.title = obj.fullTitle;
+    this.year = obj.year;
+    this.img = obj.image;
+    this.rating = obj.imDbRating;
+    this.author = "";
+    this.des = "";
+    this.star = "";
+    this.gen = "";
+    this.leng = "";
+  }
+}
